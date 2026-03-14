d1443d20aec357766576c7457e2fdffb99133832
Josue Leon <josueleon.102013@gmail.com>
2026-03-13 19:56:11 -0600

    Add Backend Directory

---
+++ Backend-Tasks/taskmanager-api/src/main/java/com/JLeonG/proyect/taskmanager/TaskmanagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.JLeonG.proyect.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerApiApplication.class, args);
+	}
+
+}
