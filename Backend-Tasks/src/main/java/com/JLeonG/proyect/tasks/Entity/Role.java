34d653ea0af63464f1eac3304888ffc7de4b6a4a
Josue Leon Avalos <josueleon.102013@gmail.com>
2026-03-15 21:28:20 -0600

    Entidad User y Task creadas, implementacion de seguridad y autenticacion.

--- Backend-Tasks/src/main/java/com/JLeonG/proyect/tasks/Entity/Role.java
+++ Backend-Tasks/src/main/java/com/JLeonG/proyect/tasks/Entity/Role.java
@@ -1,4 +1,6 @@
 package com.JLeonG.proyect.tasks.Entity;
 
-public class Role {
+public enum Role {
+    USER,
+    ADMIN
 }
--- Backend-Tasks/src/main/java/com/JLeonG/proyect/tasks/Entity/Task.java
+++ Backend-Tasks/src/main/java/com/JLeonG/proyect/tasks/Entity/Task.java
@@ -1,4 +1,42 @@
 package com.JLeonG.proyect.tasks.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    private String description;
+
+    private boolean completed = false;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
 }
